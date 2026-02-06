7117a01225287838a54096a86b5e7a777c418cd6
Lyamina14Liza <soft0010x@gmail.com>
2026-02-06 17:52:43 +0300

    добавила cardTemplate

--- Couples/script.js
+++ Couples/script.js
@@ -18,22 +18,18 @@ start_board.addEventListener('click', (event) => {
 
 function createBoard(columns) {
   gameBoard.textContent = "";
+  const template = document.querySelector('#gameTableTemplate').cloneNode(true).content;
+  const gameTable = template.querySelector('.table');
+  const restartBtn =template.querySelector('.table__button');
 
-  const template = document.getElementById('gameTableTemplate');
-  const clone = template.cloneNode;
+  gameTable.style = `
+    grid-template-columns: repeat(${columns}, 1fr);
+    grid-template-rows: repeat(${columns}, 1fr);
+  `;
+  gameBoard.append(gameTable);
 
-  const tableElem = clone.querySelector('.table');
-
-  tableElem. = 'grid';
+  restartBtn.addEventListener('click', () => {
+    location.reload();
+  });
+  gameBoard.append(restartBtn);
 }
-// function createBoard() {  
-//   const BoardSize = 8;  
-//   board = document.querySelector('.board');  
-//   board.innerHTML = '';  
-//   for (let i = 0; i < BoardSize**2; i += 1) {  
-//     const cell = document.createElement('div');  
-//     let is_white = (parseInt(i / BoardSize) + (i % BoardSize)) % 2 == 0;  
-//     cell.classList.add('cell', (is_white ? 'white' : 'black'));  
-//     board.appendChild(cell);  
-//   }  
-// }  
